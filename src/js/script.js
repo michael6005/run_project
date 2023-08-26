1c694e6c039319eb0313b5593aa0a1c5c1628320
Misha <example@gmail.com>
2023-08-26 14:22:19 +0300

    added  validation and masks

--- src/js/script.js
+++ src/js/script.js
@@ -63,4 +63,31 @@ $(document).ready(function () {
       $(".overlay, #order").fadeIn("slow");
     });
   });
+
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: "Пожалуйста, введите свое имя",
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введён адрес почты",
+        },
+      },
+    });
+  }
+
+  valideForms("#consultation-form");
+  valideForms("#consultation form");
+  valideForms("#order form");
+
+  $("input[name=phone]").mask("+7 (999) 999-9999");
 });
